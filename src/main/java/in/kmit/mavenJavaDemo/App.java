9a8b9d41842cf56d88a00fbde3af6f2b38239d2c
vamshi <vamshi24vamshi2003@gmail.com>
2024-11-29 14:56:20 +0530

    maven java commit

---
+++ src/main/java/in/kmit/mavenJavaDemo/App.java
@@ -0,0 +1,15 @@
+package in.kmit.mavenJavaDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome to Vamsee's World!" );
+        System.out.println( "sum of 5 and 6 is :"+5+6 );
+        
+    }
+}
